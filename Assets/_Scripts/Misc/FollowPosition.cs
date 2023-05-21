70aabf4d1bce203c59cab0138d35bcdc81007abf
Pitarcus <pitarcusmp@gmail.com>
2023-05-21 11:55:27 +0200

    Added textures as noises & smooth player disp

--- Assets/_Scripts/Misc/FollowPosition.cs
+++ Assets/_Scripts/Misc/FollowPosition.cs
@@ -8,11 +8,14 @@ public class FollowPosition : MonoBehaviour
     [SerializeField] bool followX;
     [SerializeField] bool followY;
     [SerializeField] bool followZ;
-    [SerializeField] bool follorFloored;
+    [SerializeField] bool followFloored;
+    [SerializeField] bool followSmooth;
+    [SerializeField] float smoothTime = 0.2f;
 
     int x, y, z;
 
     private Vector3 offset;
+    private Vector3 currentvelocity;
 
     private void Start()
     {
@@ -20,15 +23,22 @@ public class FollowPosition : MonoBehaviour
         x = followX ? 1  :0;
         y = followY ? 1 : 0;
         z = followZ ? 1 : 0;
+
+        currentvelocity = new Vector3();
     }
     // Update is called once per frame
     void Update()
     {
-        if (follorFloored)
+        if(followFloored)
         {
             transform.position = new Vector3(Mathf.Floor(target.position.x * x) + 0.5f,
                                          Mathf.Floor(target.position.y * y) + 0.5f,
                                          Mathf.Floor(target.position.z * z) + 0.5f) + offset;
+        }
+        else if(followSmooth)
+        {
+            
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(target.position.x * x, target.position.y * y, target.position.z * z) + offset, ref currentvelocity, smoothTime);
         }
         else
         {
